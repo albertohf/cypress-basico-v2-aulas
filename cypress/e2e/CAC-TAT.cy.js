37bfc17cd8927bccb0b5b560cb5827f482f1edf3
Albertohf <albertohf91@gmail.com>
2024-04-16 23:44:11 -0300

    treinamento teste cypress

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -9,11 +9,62 @@ describe("Verificar o atendimento ao cliente TAT", () => {
   });
 
   it("campos obrigatorios do formulario", () => {
+    const longText =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al";
+
     cy.get("#firstName").type("teste");
     cy.get("#lastName").type("teste");
-    cy.get("#email").type("XXXXXXXXXXXXXXX");
-    cy.get("#telefone").type("123456789");
-    cy.get("#mensagem").type("teste");
-    cy.get("#submit").click();
+    cy.get("#email").type("lallaal@gmail.com");
+    cy.get("#phone").type("123456789");
+    cy.get("#open-text-area").type(longText, { delay: 0 });
+    cy.get("button[type='submit']").click();
+
+    cy.get(".success").should("be.visible");
+  });
+
+  it("exibir mensagem ao submeter email error", () => {
+    cy.get("#firstName").type("teste");
+    cy.get("#lastName").type("teste");
+    cy.get("#email").type("lallaal");
+    cy.get("button[type='submit']").click();
+    cy.get(".error").should("be.visible");
+  });
+
+  it("verificar se campo de telefone aceita strings", () => {
+    cy.get("#phone").type("asddasd").should("have.text", "");
+  });
+
+  it("verificar se campo de telefone marcado como obrigatorio", () => {
+    cy.get("#firstName").type("teste");
+    cy.get("#lastName").type("teste");
+    cy.get("#email").type("lallaal@gmail.com");
+
+    cy.get("#phone-checkbox").click();
+
+    cy.get("button[type='submit']").click();
+    cy.get(".error").should("be.visible");
+  });
+
+  it("preencher e limpar os campos", () => {
+    cy.get("#firstName")
+      .type("alberto")
+      .should("have.value", "alberto")
+      .clear()
+      .should("have.value", "");
+    cy.get("#lastName")
+      .type("fernandes")
+      .should("have.value", "fernandes")
+      .clear()
+      .should("have.value", "");
+    cy.get("#phone")
+      .type("12334")
+      .should("have.value", "12334")
+      .clear()
+      .should("have.value", "");
+  });
+
+  it("verificar campo obrigatorio", () => {
+    cy.get("button[type='submit']").click();
+    cy.get(".error").should("be.visible");
   });
 });
